516c315fddf7ff1a74212cc972433caf63595612
Anamika1720 <panamika729@gmail.com>
2024-09-06 00:33:59 +0530

    Update Function

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 //import  Todo from "./Tasks/3.Animation Reaction/Todo"
-import Login from './Tasks/4.Netflix Login Page/login.Component';
+//import Login from './Tasks/4.Netflix Login Page/login.Component';
+import EditList from './Tasks/5.Update Products/update';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ToastContainer />
-   <Login />
+   <EditList />
   </StrictMode>,
 )
